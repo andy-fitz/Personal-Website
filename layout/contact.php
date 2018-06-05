d1dd32fbd73a70b2622639ef139e6556b2ad0763
andy-fitz <andrew.fitzgerald@ucdconnect.ie>
2018-06-05 20:09:17 +0100

    subdivided pprojects section.

---
+++ layout/contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "utf-8" >
+		<title>Andrew Fitzgerald</title>
+		<link rel = "stylesheet" type = "text/css" href = "../style/resume.css">
+	</head>
+	<body>
+	<p> Thanks for the message </p> 
+	</body>
+</html>
+	
+	
